7f683c70d2cf1010a2276a376d25d067696b95ee
Mariane <hounsounorma@gmail.com>
2023-01-14 19:57:35 +0100

    corrigé 1 du site

--- js/app.js
+++ js/app.js
@@ -7,18 +7,46 @@ $(document).ready(function(){
         items: 1,
         dots: false,
         smartSpeed: 1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
         navText: ["PREV", "NEXT"],
         responsive:{
             0:{
-                
+                nav : false
             },
-            600:{
-                
+            768:{
+                nav : true
             },
             1000:{
                 
             }
         }
+    });
+
+    $('#directeurs-slide').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:true,
+        smartSpeed: 1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        responsive:{
+            0:{
+                items: 2,
+                nav: false,
+            },
+            768:{
+                items: 2,
+            },
+            1140:{
+                items: 3,
+            }
+        }
+
     })
+
   });
   
